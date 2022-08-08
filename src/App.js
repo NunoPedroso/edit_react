4d973115a9f24adf882b18ca6e92d80da0c221ee
nuno <nuno.pedroso@cascaisambiente.pt>
2022-08-08 16:57:08 +0100

    add input search

--- src/App.js
+++ src/App.js
@@ -1,85 +1,16 @@
-import { useEffect, useState } from "react";
-
-import BasicCard from "./components/BasicCard";
-
-import errorsApiRest from "./data/errorsApiRest";
+import Form from "./components/Form";
 
 import "./styles.css";
 
 export default function App() {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(false);
-
-    const url = `https://dev.to/api/articles?top=7&per_page=3`;
-
-    const articlesArray = data?.map((article) => (
-        <BasicCard
-            key={article.id}
-            id={article.id}
-            title={article.title}
-            imgPath={article.cover_image}
-            imgSocial={article.social_image}
-            date={article.published_at}
-            description={article.description}
-            link="Read More"
-            url={article.url}
-        />
-    ));
-
-    const getArticles = () => {
-        fetch(url)
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-                return Promise.reject(response);
-            })
-            .then((result) => {
-                setData(result);
-                setError(false);
-            })
-            .catch((err) => {
-                const foundError = errorsApiRest.find((e) => e.status === err.status);
-                const errorMessage = () => {
-                    if (foundError) {
-                        return (
-                            <>
-                                <p>
-                                    Oops, something went wrong... error <b>{foundError.status}</b>{" "}
-                                    detected!
-                                </p>
-                                <p>
-                                    <b>Reason :</b> {foundError.Reason}
-                                </p>
-                                <p>
-                                    <b>Possible cause :</b> {foundError.description}
-                                </p>
-                            </>
-                        );
-                    } else {
-                        return (
-                            <p>
-                                Oops, something went wrong... <b>unknown</b> error detected!
-                            </p>
-                        );
-                    }
-                };
-                setError(errorMessage);
-                setData([]);
-            });
-    };
-
-    useEffect(() => {
-        getArticles();
-    }, []);
 
     return (
         <div className="App">
-            <h1>This week top 3 Articles</h1>
 
-            {data && <div className="articles">{articlesArray}</div>}
+            <h1>Article warehouse</h1>
 
-            {error && <span>{error}</span>}
+            <Form />
+
         </div>
     );
 }
---
+++ src/components/Form/index.js
@@ -0,0 +1,136 @@
+import "./form.css";
+import {useEffect, useState} from "react";
+
+import errorsApiRest from "../../data/errorsApiRest";
+
+import BasicCard from "../BasicCard";
+import { Oval } from  'react-loader-spinner'
+
+function Form() {
+
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false)
+
+    const [inputValue, setInputValue] = useState('')
+    const [url, setUrl] = useState(`https://dev.to/api/articles?top=7&per_page=3`)
+    const [title, setTitle] = useState('This week top 3 Articles')
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        if (inputValue){
+            setTitle(`"${inputValue}" results`)
+            setUrl(`https://dev.to/api/articles?tag=${inputValue}`)
+        } else {
+            setTitle('This week top 3 Articles')
+            setUrl(`https://dev.to/api/articles?top=7&per_page=3`)
+        }
+    }
+
+    const hadleChange = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    useEffect(() => {
+        console.log(data)
+        getArticles();
+    }, [url]);
+
+    const getArticles = () => {
+        setLoading(true)
+        fetch(url)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                return Promise.reject(response);
+            })
+            .then((result) => {
+                console.log(result)
+                setData(result);
+                setError(false);
+            })
+            .catch((err) => {
+                const foundError = errorsApiRest.find((e) => e.status === err.status);
+                const errorMessage = () => {
+                    if (foundError) {
+                        return (
+                            <>
+                                <p>
+                                    Oops, something went wrong... error <b>{foundError.status}</b>{" "}
+                                    detected!
+                                </p>
+                                <p>
+                                    <b>Reason :</b> {foundError.Reason}
+                                </p>
+                                <p>
+                                    <b>Possible cause :</b> {foundError.description}
+                                </p>
+                            </>
+                        );
+                    } else {
+                        return (
+                            <p>
+                                Oops, something went wrong... <b>unknown</b> error detected!
+                            </p>
+                        );
+                    }
+                };
+                setError(errorMessage);
+                setData([]);
+            })
+            .finally(() =>{
+                setLoading(false)
+            })
+    };
+
+    const articlesArray = data?.map((article) => (
+        <BasicCard
+            key={article.id}
+            id={article.id}
+            title={article.title}
+            imgPath={article.cover_image}
+            imgSocial={article.social_image}
+            date={article.published_at}
+            description={article.description}
+            link="Read More"
+            url={article.url}
+        />
+    ));
+
+    return (
+        <>
+
+            <form className="formData" onSubmit={handleSubmit}>
+                <input onChange={hadleChange}/>
+                <button type="submit">Search</button>
+            </form>
+
+            {(!loading) &&
+                <>
+                    {(data.length>0 && !error) && <h2 >{title}</h2>}
+
+                    {data && <div className="articles">{articlesArray}</div>}
+
+                    {data.length===0 && <h2 className="articles">No results for "{inputValue}"</h2>}
+
+                    {error && <span>{error}</span>}
+                </>
+            }
+
+            {(loading) &&
+                <div className="spinner">
+                    <Oval
+                        height = "400"
+                        width = "400"
+                        radius = "9"
+                        color = 'green'
+                        ariaLabel = 'three-dots-loading'
+                    />
+                </div>
+            }
+        </>
+    );
+}
+
+export default Form;
