7d0299ceef2372fa662874d732b36d373fd7b498
nuno <nuno.pedroso@cascaisambiente.pt>
2022-08-09 23:06:41 +0100

    ADD router
    # Conflicts:
    #	src/components/Form/index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import Form from "./components/Form";
 
-import "./styles.css";
+import  { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {NavLink} from "react-router-dom";
+import Home from "./pages/Home";
+
+
+import './index.css'
+import './styles.css'
+import About from "./pages/About";
+
 
 export default function App() {
 
     return (
         <div className="App">
-
-            <h1>Article warehouse</h1>
-
-            <Form />
+        <h1>React Router</h1>
+        <Router>
+            <header>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+            </header>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/about" element={<About/>} />
+            </Routes>
+        </Router>
 
         </div>
     );
---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import Form from "../../components/Form";
+import '../../index.css'
+
+function Home () {
+    return (
+        <div className="App">
+
+            <h1>Article warehouse</h1>
+
+            <Form />
+
+        </div>
+    );
+}
+
+export default Home
